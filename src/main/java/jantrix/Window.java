40a663e71cdbefd6da183453a823016d6bb3a980
Alejandro <alejandroruperez@gmail.com>
2018-12-20 09:46:58 +0100

    Simplified loading of graphics

--- src/main/java/jantrix/Window.java
+++ src/main/java/jantrix/Window.java
@@ -10,26 +10,28 @@ public class Window extends JFrame {
     private final int OPT_SELECTLEVEL = 1;
     private final int OPT_MAXSCORES = 2;
     private final int OPT_EXIT = 3;
-    
+
     private final int MENU = 0;
     private final int GAME = 1;
     private final int EXIT = 2;
-    
+
     private Image menuBackg;
     private Image gameBackg;
     private Image icon;
     private Image[] tile = new Image[9];
     private Image currentBackg;
-    
+
     private KeybListener keybl = new KeybListener();
     private BufferStrategy bf;
     private Menu menu = null;
     private Game game = null;
-    
+
     private int i;
     private int exitCode = OPT_REMAIN;
     private int jantrixState = MENU;
-    
+
+    private String graphicsPath = "./src/main/resources/graphics";
+
     public Window() {
         //Initializes the game window
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -37,42 +39,53 @@ public class Window extends JFrame {
         this.setTitle("Jantrix v0.8");
         this.setSize(377,520);
         this.setResizable(false);
-        
-        //Load the game graphics
-        try {
-            menuBackg = getToolkit().getImage("./src/main/resources/graphics/presentacion2.png");
-            gameBackg = getToolkit().getImage("./src/main/resources/graphics/fondo.png");
-            icon = getToolkit().getImage("./src/main/resources/graphics/icono.png");
-            for(i=1; i<8; i++) {
-                tile[i] = getToolkit().getImage("./src/main/resources/graphics/" + i + ".png");
-            }
-            
-        } catch(Exception e) {System.out.println("Graphics hasn't been loaded");}
-        
+
+        loadGraphics();
+
         this.setIconImage(icon);
         this.setVisible(true);
-        
+
         this.createBufferStrategy(2);
         this.setLocation(100,100);
         this.setVisible(true);
-        
+
         bf = this.getBufferStrategy();
-        
+
         jantrixLoop();
     }
-    
+
+    private void loadGraphics() {
+        try {
+            menuBackg = loadGraphic("presentacion2");
+            gameBackg = loadGraphic("fondo");
+            icon = loadGraphic("icono");
+
+            for(i = 1; i < 8; i++) {
+                tile[i] = loadGraphic(Integer.toString(i));
+            }
+        } catch(Exception e) {
+            System.out.println("Graphics hasn't been loaded");
+        }
+    }
+
+    private Image loadGraphic(String graphicName) {
+        String graphicPath = graphicsPath + "/" + graphicName + ".png";
+
+        return getToolkit().getImage(graphicPath);
+    }
+
     private void jantrixLoop() {
         while(jantrixState != EXIT) {
             switch(jantrixState) {
                 case MENU:
                     game = null;
                     currentBackg = menuBackg;
-                    
+
                     menu = new Menu(keybl,this);
                     if(exitCode == OPT_NEWGAME) jantrixState = GAME;
                     else if(exitCode == OPT_EXIT) jantrixState = EXIT;
                     break;
-                
+
                 case GAME:
                     menu = null;
                     currentBackg = gameBackg;
@@ -82,7 +95,7 @@ public class Window extends JFrame {
             }
         }
     }
-    
+
     public BufferStrategy getBf() {return bf;}
     public Image getBackg() {return currentBackg;}
     public void setExitCode(int code) {exitCode = code;}
